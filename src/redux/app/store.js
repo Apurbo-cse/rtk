320f82845fc4cb9f5da4c58b1db738cbd6fffb25
Apurbo Kumar Anup <apurboka@gmail.com>
2023-08-07 17:11:27 +0600

    Concept - redux-logger

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from '../features/counter/counterSlice'; 
 import dynamicCounterReducer from '../features/counter/dynamicCounterSlice'; 
+import { createLogger } from "redux-logger";
+
+const logger = createLogger()
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         dynamicCounter: dynamicCounterReducer
-    }
+    },
+    middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(logger)
 })
 
 export default store;
